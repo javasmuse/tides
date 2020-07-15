a98936e5f1a7bd2177649f3243c3103b09a4241d
Candace Lewis <candace@Mac-mini.local>
2020-07-14 20:17:45 -0400

    before paul corupts my work

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const tideD = document.getElementById('tide-display');
 
 // Send GET to API to retrieve tides
-const purple = async function getTides() {
+async function getTides() {
     const res = await fetch('https://tidesandcurrents.noaa.gov/api/datagetter?date=today&station=8729840&product=water_level&datum=STND&time_zone=lst_ldt&units=english&format=json');
     const data = await res.json();
+
     return data;
+    // const tides = data.metadata.name;
+    // console.log(tides);
 }
 
-getTides().then(data => console.log(data));
-tideD = purple.station;
+getTides().then(data => console.log(data.metadata.name));
+
 
 
 // fetch('https://tidesandcurrents.noaa.gov/api/datagetter?date=today&station=8729840&product=water_level&datum=STND&time_zone=lst_ldt&units=english&format=json').then(function (response) {
